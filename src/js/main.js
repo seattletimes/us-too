29603e4b3b6d4b830c520eecf50c081b41cde78d
Thomas Wilburn <news@thomaswilburn.net>
2018-04-02 11:07:54 -0700

    Enable caption animations, publish to staging

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,20 @@
 // var track = require("./lib/tracking");
 
 require("component-responsive-frame/child");
+
+document.body.classList.add("javascript");
+
+var $ = require("./lib/qsa");
+var debounce = require("./lib/debounce");
+
+var captions = $(".caption");
+
+var onScroll = function() {
+  captions = captions.filter(function(c) {
+    var bounds = c.getBoundingClientRect();
+    if (bounds.top > window.innerHeight * .8) return true;
+    c.classList.add("seen");
+  });
+};
+
+window.addEventListener("scroll", debounce(onScroll, 200));
