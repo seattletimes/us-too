26ffa2f08ed4a942f1c88b7fb6b88503d622eefe
Thomas Wilburn <news@thomaswilburn.net>
2018-04-04 10:12:05 -0700

    Tweaks, hide Comscore pixel

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,6 @@
 require("./lib/ads");
-// var track = require("./lib/tracking");
 var paywall = require("./lib/paywall");
 setTimeout(() => paywall(10777198), 1000);
-
-require("component-responsive-frame/child");
 
 document.body.classList.add("javascript");
 
